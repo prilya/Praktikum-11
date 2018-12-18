3ded6c91ea5f48ee262eb07016f5e24a43b01197
prilya <36910661+prilya@users.noreply.github.com>
2018-12-18 14:27:45 +0700

    Add files via upload

---
+++ Demo2.java
@@ -0,0 +1,14 @@
+package Praktikum11;
+
+/**
+ *
+ * @author marsheila
+ */
+public class Demo2 {
+    public static void main(String[] args){
+        String teks="Java";
+        System.out.println("teks = Java");
+        System.out.println("equals(JAVA) = "+teks.equals("JAVA"));
+        System.out.println("equalsIgnoreCase(JAVA) = "+teks.equalsIgnoreCase("JAVA"));
+    }
+}
